7b0eb56739a6f5ba1f0d3003669f745940c12a52
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-21 11:40:16 -0700

    removing the console log of my api key

--- src/ImgComp.js
+++ src/ImgComp.js
@@ -1,32 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import DomCard from './DomCard';
-import { prettyDOM } from '@testing-library/react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DomCard from "./DomCard";
+import { prettyDOM } from "@testing-library/react";
 
 const ImgComp = (props) => {
-    const [imgInfo, setImgInfo] = useState([]);
+  const [imgInfo, setImgInfo] = useState([]);
 
-    const api_key = process.env.REACT_APP_API_KEY;
-    console.log(api_key)
+  const api_key = process.env.REACT_APP_API_KEY;
 
-    const EffectFn = () => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
-        .then((res) => {
-            let info = res.data;
-            console.log(info)
-            setImgInfo(info)
-        })
-        .catch((error) => {
-            return error;
-        })
-    }
-    useEffect(EffectFn, [])
-    
-    return(
-        <div>
-            <DomCard imgInfo={imgInfo}/>
-        </div>
-    )
-}
+  const EffectFn = () => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
+      .then((res) => {
+        let info = res.data;
+        console.log(info);
+        setImgInfo(info);
+      })
+      .catch((error) => {
+        return error;
+      });
+  };
+  useEffect(EffectFn, []);
+
+  return (
+    <div>
+      <DomCard imgInfo={imgInfo} />
+    </div>
+  );
+};
 
 export default ImgComp;
